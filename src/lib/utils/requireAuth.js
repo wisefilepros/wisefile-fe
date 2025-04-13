b868c220b3bd7fc84440032bdfc06b84245df9fa
Ben <ben.a.burgos@gmail.com>
2025-04-13 11:59:38 -0700

    working through session logic

--- src/lib/utils/requireAuth.js
+++ src/lib/utils/requireAuth.js
@@ -3,6 +3,6 @@ import { redirect } from '@sveltejs/kit';
 export function requireAuth(cookies) {
 	const token = cookies.get('accessToken');
 	if (!token) {
-		throw redirect(302, '/login');
+		throw redirect(302, '/');
 	}
 }
--- src/routes/app/cases/+page.svelte
+++ src/routes/app/cases/+page.svelte
@@ -2,3 +2,5 @@
 	/** @type {{ data: import('./$types').PageData }} */
 	let { data } = $props();
 </script>
+
+<h1>Cases</h1>
