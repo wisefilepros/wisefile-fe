91aba4e232a059c685dae499aad329f1b601e4e7
Ben <ben.a.burgos@gmail.com>
2025-04-13 10:32:04 -0700

    adds role check to admin route

--- src/routes/admin/+layout.server.js
+++ src/routes/admin/+layout.server.js
@@ -1,5 +1,7 @@
 import { requireAuth } from '$lib/utils/requireAuth';
+import { requireRole } from '$lib/utils/requireRole';
 
 export function load({ cookies }) {
 	requireAuth(cookies);
+	requireRole(cookies, 'admin');
 }
