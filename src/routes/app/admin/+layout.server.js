261f849a56a9391e5e776e7254e46d6e04ca8c1c
Ben <ben.a.burgos@gmail.com>
2025-04-20 00:29:11 -0700

    removes jwt

--- src/routes/app/admin/+layout.server.js
+++ src/routes/app/admin/+layout.server.js
@@ -1,7 +1,5 @@
 import { requireAuth } from '$lib/utils/requireAuth';
-import { requireRole } from '$lib/utils/requireRole';
 
 export function load({ cookies }) {
 	requireAuth(cookies);
-	requireRole(cookies, 'admin');
 }
