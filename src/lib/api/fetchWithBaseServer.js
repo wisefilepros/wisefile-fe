5ce0842b02a26f1157c017db0b7162303125340d
Ben <ben.a.burgos@gmail.com>
2025-04-20 00:56:32 -0700

    adds base env

--- src/lib/api/fetchWithBaseServer.js
+++ src/lib/api/fetchWithBaseServer.js
@@ -1,5 +1,7 @@
+const BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
 export async function apiFetchServer(path, { cookie = '', ...options } = {}) {
-	const res = await fetch(`https://wisefile-be.onrender.com${path}`, {
+	const res = await fetch(`${BASE_URL}${path}`, {
 		method: options.method || 'GET',
 		headers: {
 			'Content-Type': 'application/json',
