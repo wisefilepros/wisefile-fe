65e6e4c1509046d5216a80608c3e521b8279f9f5
Ben <ben.a.burgos@gmail.com>
2025-04-20 01:27:30 -0700

    adds test

--- src/lib/utils/requireAuth.js
+++ src/lib/utils/requireAuth.js
@@ -1,6 +1,7 @@
 import { redirect } from '@sveltejs/kit';
 
 export function requireAuth(cookies) {
+	console.log('🔐 accessToken on SSR:', token);
 	const token = cookies.get('accessToken');
 	if (!token) {
 		throw redirect(302, '/');
