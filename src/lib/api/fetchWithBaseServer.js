24e6b20c3aeb3c53bc7697485ff527eb806d6bec
Ben <ben.a.burgos@gmail.com>
2025-06-01 10:47:50 -0700

    fixes fetch

--- src/lib/api/fetchWithBaseServer.js
+++ src/lib/api/fetchWithBaseServer.js
@@ -1,11 +1,11 @@
 const BASE_URL = import.meta.env.VITE_API_BASE_URL_SSR;
 
-export async function apiFetchServer(path, { cookie = '', ...options } = {}) {
+export async function apiFetchServer(path, options = {}, { raw = false } = {}) {
 	const res = await fetch(`${BASE_URL}${path}`, {
 		method: options.method || 'GET',
 		headers: {
 			'Content-Type': 'application/json',
-			...(cookie ? { cookie } : {}),
+			...(options.cookie ? { cookie: options.cookie } : {}),
 			...options.headers
 		}
 	});
@@ -15,5 +15,5 @@ export async function apiFetchServer(path, { cookie = '', ...options } = {}) {
 		throw new Error(err.message || 'Request failed');
 	}
 
-	return res.json();
+	return raw ? res : res.json();
 }
